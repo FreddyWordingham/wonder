1bd76a310a9d459f02f3840760ffaff8b2a837a9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-09-11 22:46:29 +0100

    Attempting path name fix.

--- web/sprites.js
+++ web/sprites.js
@@ -21,7 +21,7 @@ function load_img(name) {
     img.onload = function () {
         console.log("Loaded image: ", name);
     };
-    img.src = './master/res/sprites/' + name + '.png';
+    img.src = './wonder/res/sprites/' + name + '.png';
 
     return img;
 }
