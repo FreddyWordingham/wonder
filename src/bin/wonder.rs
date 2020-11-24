092aa33015377d9ef90b456ad258b0bd519bc27b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-24 18:59:04 +0000

    Cleaned up main.

--- src/bin/wonder.rs
+++ src/bin/wonder.rs
@@ -2,18 +2,24 @@ use arctk::game::{Map, State};
 use rltk::{main_loop, RltkBuilder};
 
 fn main() -> rltk::BError {
+    // Rendering window.
     let width = 80;
     let height = 50;
-
     let context = RltkBuilder::simple(width, height)?
         .with_title("Roguelike - Wonder")
         .build()?;
-    let mut gs = State::new(Map::new([width, height]));
 
+    // Map to play.
+    let map = Map::new_random([width, height]);
+    // Game state.
+    let mut gs = State::new(map);
+
+    // Add entities.
     gs.add_player((width / 2) as i32, (height / 3) as i32);
     for i in 0..10 {
         gs.add_enemy(i as i32 * 3, (2 * height as i32) / 3);
     }
 
+    // Run the main loop.
     main_loop(context, gs)
 }
