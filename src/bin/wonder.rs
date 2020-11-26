d554985efb9c55e1ff5c0bad5be074a24bed07bb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-26 01:14:47 +0000

    Updated main to reflect library updates.

--- src/bin/wonder.rs
+++ src/bin/wonder.rs
@@ -1,21 +1,26 @@
-use arctk::game::{Map, State};
-use rltk::{main_loop, RltkBuilder};
+use arctk::game::{cartographer, State};
+use rltk::{main_loop, RandomNumberGenerator, RltkBuilder};
 
 fn main() -> rltk::BError {
     // Rendering window.
-    let width = 80;
-    let height = 50;
+    let width = 128;
+    let height = 128;
     let context = RltkBuilder::simple(width, height)?
         .with_title("Roguelike - Wonder")
         .build()?;
 
-    // Map to play.
-    let map = Map::new_random([width, height]);
-    // Game state.
+    // Resources.
+    let mut rng = RandomNumberGenerator::new();
+
+    // Map generation.
+    let map = cartographer::forest(&mut rng);
+    let map = cartographer::caves(&mut rng);
+
+    let spawn = map.player_spawn;
     let mut gs = State::new(map);
 
     // Add entities.
-    gs.add_player((width / 2) as i32, (height / 3) as i32);
+    gs.add_player(spawn.x, spawn.y);
     for i in 0..10 {
         gs.add_enemy(i as i32 * 3, (2 * height as i32) / 3);
     }
